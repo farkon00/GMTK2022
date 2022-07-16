cace2a7444b4595e65edc5a543283be065c4fcbf
farkon00 <sammer2016sammer@gmail.com>
2022-07-16 15:52:59 +0300

    Fixed new location

---
+++ scripts/Boss3.gd
@@ -0,0 +1,15 @@
+extends "BaseBoss.gd"
+
+export var speed: float = 200
+
+var move_dir = 0
+
+func check_move(delta):
+	var dist = global_position.x - $"../Player".global_position.x
+    if abs(dist) < 30:
+        return
+	move_dir = -sign(dist)
+	position.x += speed * delta * move_dir
+
+func _process(delta):
+    check_move(delta)
