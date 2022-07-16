0543f6117e51b50cca07991bdfe2fe152e1c412b
farkon00 <sammer2016sammer@gmail.com>
2022-07-16 17:02:56 +0300

    Polished 3rd boss, added attack

--- scripts/Boss3.gd
+++ scripts/Boss3.gd
@@ -1,12 +1,13 @@
 extends "BaseBoss.gd"
 
 export var speed: float = 200
-export var tp_time: float = 1
-export var tp_cooldown: float = 1
+export var vert_speed: float = 40
 
 var move_dir = 0
-var is_tp = false
-var tp_timer: float = 0
+
+func _attack_area_entered(body):
+	if body.is_in_group("Player"):
+		body.damage()
 
 func check_move(delta):
 	var dist = global_position.x - $"../Player".global_position.x
@@ -15,22 +16,13 @@ func check_move(delta):
 	move_dir = -sign(dist)
 	position.x += speed * delta * move_dir
 
-func check_tp(delta):
-	if tp_timer > 0:
-		tp_timer -= delta
+func check_vmove(delta):
+	var dist = global_position.y - $"../Player".global_position.y
+	if abs(dist) < 30:
 		return
-	var dist_vec = (global_position - $"../Player".global_position).abs()
-	if dist_vec.x > 250 or dist_vec.y > 170:
-		is_tp = true
-		position.x = 100000000
-		var tp_pos = $"../Player".global_position
-		yield(get_tree().create_timer(tp_time), "timeout")
-		global_position = tp_pos 
-		is_tp = false
-		tp_timer = tp_cooldown
+	move_dir = -sign(dist)
+	position.y += vert_speed * delta * move_dir
 
 func _process(delta):
-	if is_tp:
-		return
 	check_move(delta)
-	check_tp(delta)
+	check_vmove(delta)
